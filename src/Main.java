87eab424daa5f6cc2a7202f8b96a4c2a26bbe09b
Ezaitsev <jackhares.mai@gmail.com>
2019-09-29 15:49:58 +0300

    реализована логика поиска всех вариантов

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String[][] matr = {
+                {"1", "2", "3"},
+                {"4","5","6"},
+                {"7","8","9"},
+                {"","0",""}
+        };
+        String chislo;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] x = reader.readLine().toCharArray();
+
+        ArrayList list = new ArrayList();
+
+
+        for (int index = 0; index < x.length; index++){
+            //TODO: избавиться от char
+            chislo = String.valueOf(x[index]);
+
+            x : for (int i = 0; i < matr.length; i++){
+                for (int j = 0; j < matr[i].length; j++){
+
+                    if (matr[i][j].equals(chislo) && !matr[i][j].isEmpty()){
+//                        list.add(matr[i][j]);
+
+                        if ( (i + 1) < matr.length && !matr[i + 1][j].isEmpty()){
+//                            list.add(matr[i + 1][j]);
+                            chislo += matr[i + 1][j];
+                        } if ( (i - 1) >= 0 && !matr[i - 1][j].isEmpty()){
+//                            list.add(matr[i - 1][j]);
+                            chislo += matr[i - 1][j];
+                        }
+                        if (j + 1 < matr[i].length && !matr[i][j + 1].isEmpty()){
+//                            list.add(matr[i][j + 1]);
+                            chislo += matr[i][j + 1];
+                        } if ( (j - 1) >= 0 && !matr[i][j - 1].isEmpty()){
+//                            list.add(matr[i][j - 1]);
+                            chislo += matr[i][j - 1];
+                        }
+
+                        list.add(chislo);
+                        break x;
+                    }
+
+                }
+            }
+
+        }
+
+
+        System.out.println(list.toString());
+
+        //обход по лиссту
+        for (int ind  =0; ind < list.size(); ind++ ){
+
+
+
+        }
+
+
+    }
+
+
+}
